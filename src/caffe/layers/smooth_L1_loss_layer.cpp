5243b4616dffd063b2389baaa2cdff955784d1a6
xuehan xiong <xuehan.xiong@snapchat.com>
2016-08-12 13:53:17 -0700

    allow dimension mismatch in smooth_l1_loss

--- src/caffe/layers/smooth_L1_loss_layer.cpp
+++ src/caffe/layers/smooth_L1_loss_layer.cpp
@@ -19,9 +19,9 @@ namespace caffe {
     void SmoothL1LossLayer<Dtype>::Reshape(
                                            const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
         LossLayer<Dtype>::Reshape(bottom, top);
-        CHECK_EQ(bottom[0]->channels(), bottom[1]->channels());
-        CHECK_EQ(bottom[0]->height(), bottom[1]->height());
-        CHECK_EQ(bottom[0]->width(), bottom[1]->width());
+//        CHECK_EQ(bottom[0]->channels(), bottom[1]->channels());
+//        CHECK_EQ(bottom[0]->height(), bottom[1]->height());
+//        CHECK_EQ(bottom[0]->width(), bottom[1]->width());
         if (has_weights_) {
             CHECK_EQ(bottom[0]->channels(), bottom[2]->channels());
             CHECK_EQ(bottom[0]->height(), bottom[2]->height());
